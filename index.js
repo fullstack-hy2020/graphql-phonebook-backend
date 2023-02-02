5adf70925cec9338104598754e4249bfc96051e0
Matti Luukkainen <mluukkai@iki.fi>
2023-02-02 20:11:58 +0200

    part8-3

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ const typeDefs = `
     city: String! 
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+  
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+  }
+
   type Person {
     name: String!
     phone: String
@@ -52,13 +63,25 @@ const typeDefs = `
       street: String!
       city: String!
     ): Person
+
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }
 `
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
   },
@@ -73,23 +96,27 @@ const resolvers = {
   Mutation: {
     addPerson: (root, args) => {
       if (persons.find(p => p.name === args.name)) {
-        /*
-        throw new UserInputError('Name must be unique', {
-          invalidArgs: args.name,
-        })
-        */
         throw new GraphQLError('Name must be unique', {
           extensions: {
             code: 'BAD_USER_INPUT',
             invalidArgs: args.name
-          },
+          }
         })
       }
-
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
       return person
-    }
+    },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if (!person) {
+        return null
+      }
+  
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    } 
   }
 }
 
