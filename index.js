abb5893a0cf2cefed552b6ef160e1eac92c19507
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-10-13 18:01:08 +0300

    Add part8-1

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ let persons = [
 ]
 
 const typeDefs = `
+  type Address {
+    street: String!
+    city: String! 
+  }
+
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String! 
+    address: Address!
     id: ID!
   }
 
@@ -45,6 +49,14 @@ const resolvers = {
     personCount: () => persons.length,
     allPersons: () => persons,
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+  Person: {
+    address: ({ street, city }) => {
+      return {
+        street,
+        city,
+      }
+    },
   },
 }
 
