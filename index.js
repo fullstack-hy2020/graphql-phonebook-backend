d1e5d76ffa479b0e654a3df4f59f0c3c9c1355a9
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-11-22 14:30:22 +0200

    Add part8-3

--- index.js
+++ index.js
@@ -39,9 +39,14 @@ const typeDefs = /* GraphQL */ `
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -52,12 +57,20 @@ const typeDefs = /* GraphQL */ `
       street: String!
       city: String!
     ): Person
+    editNumber(name: String!, phone: String!): Person
   }
 `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     allPersons: () => persons,
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
@@ -83,6 +96,16 @@ const resolvers = {
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
       return person
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name)
+      if (!person) {
+        return null
+      }
+
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
+      return updatedPerson
     },
   },
 }
