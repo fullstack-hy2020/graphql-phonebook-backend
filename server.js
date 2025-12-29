6e17c446b3864d8121d563eb5190ceea577de66f
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-12-29 21:20:42 +0200

    Add part8-6

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
 const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer')
+const { expressMiddleware } = require('@as-integrations/express5')
+const cors = require('cors')
+const express = require('express')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const http = require('http')
 const jwt = require('jsonwebtoken')
 
 const resolvers = require('./resolvers')
 const typeDefs = require('./schema')
 const User = require('./models/user')
 
-const startServer = (port) => {
+const startServer = async (port) => {
+  const app = express()
+  const httpServer = http.createServer(app)
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   })
-
-  startStandaloneServer(server, {
-    listen: { port },
-    context: async ({ req, res }) => {
-      const auth = req ? req.headers.authorization : null
-      if (auth && auth.startsWith('Bearer ')) {
-        const decodedToken = jwt.verify(
-          auth.substring(7),
-          process.env.JWT_SECRET,
-        )
-        const currentUser = await User.findById(decodedToken.id).populate(
-          'friends',
-        )
-        return { currentUser }
-      }
-    },
-  }).then(({ url }) => {
-    console.log(`Server ready at ${url}`)
-  })
+  await server.start()
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(
+            auth.substring(7),
+            process.env.JWT_SECRET,
+          )
+          const currentUser = await User.findById(decodedToken.id).populate(
+            'friends',
+          )
+          return { currentUser }
+        }
+      },
+    }),
+  )
+  httpServer.listen(port, () =>
+    console.log(`Server is now running on http://localhost:${port}`),
+  )
 }
 
 module.exports = startServer
