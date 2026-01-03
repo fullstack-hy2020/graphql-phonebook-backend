9d0dab8d3edad2fbd2e572af83266eb410880781
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2026-01-03 12:02:36 +0200

    Refactor context definition

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const resolvers = require('./resolvers')
 const typeDefs = require('./schema')
 const User = require('./models/user')
 
+const getUserFromAuthHeader = async (auth) => {
+  if (!auth || !auth.startsWith('Bearer ')) {
+    return null
+  }
+
+  const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+  return User.findById(decodedToken.id).populate('friends')
+}
+
 const startServer = (port) => {
   const server = new ApolloServer({
     typeDefs,
@@ -14,18 +23,10 @@ const startServer = (port) => {
 
   startStandaloneServer(server, {
     listen: { port },
-    context: async ({ req, res }) => {
-      const auth = req ? req.headers.authorization : null
-      if (auth && auth.startsWith('Bearer ')) {
-        const decodedToken = jwt.verify(
-          auth.substring(7),
-          process.env.JWT_SECRET,
-        )
-        const currentUser = await User.findById(decodedToken.id).populate(
-          'friends',
-        )
-        return { currentUser }
-      }
+    context: async ({ req }) => {
+      const auth = req.headers.authorization
+      const currentUser = await getUserFromAuthHeader(auth)
+      return { currentUser }
     },
   }).then(({ url }) => {
     console.log(`Server ready at ${url}`)
